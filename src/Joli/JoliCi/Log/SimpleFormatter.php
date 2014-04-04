9a551c8938f61ef74beaff3737b8dfa7c2091b9e
Joel Wurtz <jwurtz@jolicode.com>
2014-04-04 12:46:05 +0200

    Add static progress bar when getting image from docker

--- src/Joli/JoliCi/Log/SimpleFormatter.php
+++ src/Joli/JoliCi/Log/SimpleFormatter.php
@@ -14,13 +14,44 @@ use Monolog\Formatter\FormatterInterface;
 
 class SimpleFormatter implements FormatterInterface
 {
+    private $static = array();
 
     /*
      * (non-PHPdoc) @see \Monolog\Formatter\FormatterInterface::format()
      */
     public function format(array $record)
     {
-        return $record['message'];
+        $message = $record['message'];
+
+        if (isset($record['context']['static']) && $record['context']['static']) {
+            $id      = $record['context']['static-id'];
+
+            if (!isset($this->static[$id])) {
+                $this->static[$id] = array(
+                    'current_line' => count($this->static),
+                    'message'      => $message
+                );
+
+                $message = sprintf("\n%s", $message);
+            } else {
+                $diff                         = (count($this->static) - $this->static[$id]['current_line']) - 1;
+                $lastMessage                  = $this->static[$id]['message'];
+                $this->static[$id]['message'] = $message;
+
+                //Add space to replace old string in message
+                if (mb_strlen($lastMessage) > mb_strlen($message)) {
+                    $message = str_pad($message, mb_strlen($lastMessage), " ", STR_PAD_RIGHT);
+                }
+
+                if ($diff > 0) {
+                    $message = sprintf("\x0D\x1B[%sA%s\x1B[%sB", $diff, $message, $diff);
+                } else {
+                    $message = "\x0D".$message;
+                }
+            }
+        }
+
+        return $message;
     }
 
     /*
