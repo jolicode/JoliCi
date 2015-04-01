092284d1cad054b0087d93906a4e101d007f553e
Joel Wurtz <jwurtz@jolicode.com>
2015-04-01 15:02:27 +0200

    Add update image command

---
+++ src/Joli/JoliCi/Command/UpdateImageCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Joli\JoliCi\Command;
+
+use Joli\JoliCi\Container;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class UpdateImageCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('image-update');
+        $this->setDescription('Update docker images used to build test environnement');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = new Container();
+        $docker = $container->getDocker();
+        $logger = $container->getLoggerCallback((OutputInterface::VERBOSITY_VERBOSE <= $output->getVerbosity()));
+
+        foreach ($docker->getImageManager()->findAll() as $image) {
+            if (preg_match('#^jolicode/(.+?)$#', $image->getRepository())) {
+                $docker->getImageManager()->pull($image->getRepository(), 'latest', $logger->getBuildCallback());
+            }
+        }
+    }
+} 
